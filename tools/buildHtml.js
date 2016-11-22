2d7a336fb2201d0c995c8aa03b6edb7a47add7e4
michael.wei <michael.wei@foxmail.com>
2016-11-21 21:27:18 -0500

    check in from codeanywhere

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,4 +1,4 @@
-// This script copies src/index.html into /dist/index.html
+
 // This is a good example of using Node and cheerio to do a simple file transformation.
 // In this case, the transformation is useful since we only use a separate css file in prod.
 import fs from 'fs';
